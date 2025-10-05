92c8542ac19d4a9b5f841880a062314d2ece53cc
Anindita Dey <aninditadey@Aninditas-MacBook-Air.local>
2025-10-05 21:57:40 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='font-bold flex justify-center p-10 text-3xl '>Firebase</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB5gPcdW3lSfGKrXX8wU3c5aXSzsTrFT-A",
+  authDomain: "fir-course-3bcb6.firebaseapp.com",
+  projectId: "fir-course-3bcb6",
+  storageBucket: "fir-course-3bcb6.firebasestorage.app",
+  messagingSenderId: "612297577269",
+  appId: "1:612297577269:web:2da386bc9b57af6343727c",
+  measurementId: "G-TN1GFS3JBG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
